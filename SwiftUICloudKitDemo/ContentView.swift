e28f2d273e71cd40bdefe0cd58307ac8576449a5
Alex <therebeloper@gmail.com>
2019-09-22 21:04:56 +0300

    Initial Commit

---
+++ SwiftUICloudKitDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICloudKitDemo
+//
+//  Created by Alex Nagy on 22/09/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
